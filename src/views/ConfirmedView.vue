8948dbb62e7608ee072887999301661e5ef63701
Mauricio <cancinocontrerasm@gmail.com>
2023-07-20 18:40:57 -0400

    button fixed

--- src/views/ConfirmedView.vue
+++ src/views/ConfirmedView.vue
@@ -22,6 +22,9 @@ export default {
     getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
+    redirectTo(nameRoute){
+        this.$router.push({name:nameRoute})
+    }
   },
 };
 </script>
